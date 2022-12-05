bd5eceb35b94f215502f32d897fc8453f2d9865e
BMarelli <marellibautista@gmail.com>
2022-12-05 19:55:57 -0300

    comment some code

--- src/util/api.ts
+++ src/util/api.ts
@@ -11,6 +11,12 @@ export class Fetcher {
   private public_key = "074de923ec9ffafe7651fb0668947078"
   private private_key = "9f60cc8d6d8dac104f166f1b5a805909ef27ddf8"
 
+  /*
+    Abstraction of the fetch method to make it easier to use
+    @param endpoint: string - the endpoint to fetch from
+    @param offset: number | undefined - the offset to fetch from
+    @returns Promise<T> - the response from the fetch
+  */
   private async fetch<T>(endpoint: string, offset?: number): Promise<T> {
     let ts: number = Date.now()
     let hash = md5(`${ts}${this.private_key}${this.public_key}`);
@@ -27,6 +33,11 @@ export class Fetcher {
     return await response.json()
   }
 
+  /*
+    Parses the error from the response
+    @param response: Response - the response to parse
+    @returns Promise<string | undefined> - the error message or undefined
+  */
   private async parseErrorFromResponse(response: Response): Promise<string | undefined> {
     try {
       return (await response.json()).status
@@ -35,6 +46,11 @@ export class Fetcher {
     }
   }
 
+  /*
+    Fetches the comics from the Marvel API
+    @param offset: number | undefined - the offset to fetch from
+    @returns Promise<ComicInterface[]> - the comics from the API
+  */
   async getComics(offset?: number): Promise<ComicInterface[]> {
     const response = await this.fetch<ApiResponse<ComicInterface[]>>(':443/v1/public/comics?format=comic&', offset)
 
@@ -49,6 +65,11 @@ export class Fetcher {
     })
   }
 
+  /*
+    Fetches a specific comic from the Marvel API
+    @param id: number - the id of the comic to fetch
+    @returns Promise<ComicInterface> - the comic from the API
+  */
   async getComic(id: number): Promise<ComicInterface> {
     const response = await this.fetch<ApiResponse<ComicInterface[]>>(`:443/v1/public/comics/${id}?`)
     const comic = response.data.results[0]
@@ -62,6 +83,11 @@ export class Fetcher {
     }
   }
 
+  /*
+    Fetches a specific character from the Marvel API
+    @param id: number - the id of the character to fetch
+    @returns Promise<CharacterInterface> - the character from the API
+  */
   async getCharacter(id: number): Promise<CharacterInterface> {
     const response = await this.fetch<ApiResponse<CharacterInterface[]>>(`:443/v1/public/characters/${id}?`)
     const character = response.data.results[0]
